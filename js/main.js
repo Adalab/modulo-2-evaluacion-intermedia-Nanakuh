131e6ae793206d7a7f88ecc738d0407c4721d5ac
Ana Gil <anagilrodrig@gmail.com>
2023-12-09 10:29:47 +0100

    Declare constants

--- js/main.js
+++ js/main.js
@@ -1,4 +1,17 @@
 'use strict'
 
-const trackButton = document.querySelector('.js__trackButton');
-const track = document.querySelector('.js__track')
+const number = document.querySelector('.js_number')
+const testButton = document.querySelector('.js_testButton')
+const track = document.querySelector('.js_track');
+const attempts = document.querySelector('.js_attempts')
+
+
+testButton.addEventListener('click')
+  
+   
+
+
+function getRandomNumber(max) {
+    return Math.ceil(Math.random() * max);
+    }
+    console.log(getRandomNumber);
